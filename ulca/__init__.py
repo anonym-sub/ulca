26074ae99714d660ee31d83cbfdd4e30528084e3
anonym-sub <imosweet@hotmail.com>
2021-03-28 22:04:23 -0700

    Add files via upload

---
+++ ulca/__init__.py
@@ -0,0 +1,10 @@
+__author__ = "Anonymous author(s)"
+__copyright__ = "Copyright 2021 Anonymous author(s)"
+__license__ = "BSD 3-Clause 'New' or 'Revised' License"
+__URL__ = "https://bitbucket.org/anon-sub/ulca/src/master/"
+
+import sys
+
+__all__ = [
+    'ulca', 'evd_ulca', '__author__', '__copyright__', '__license__', '__URL__'
+]
